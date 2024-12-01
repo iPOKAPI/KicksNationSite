b245981cde888d56848b328031956c820952aeca
Lusam Delictor <142870497+GreyWingX1@users.noreply.github.com>
2024-12-01 23:58:36 +0800

    Delete userhomepage directory

--- userhomepage/about.js
+++
@@ -1,7 +0,0 @@
-function confirmDataLoss() {
-    if (confirm("Reloading this page leads to data loss. Are you sure?")) {
-      return true; // Proceed to cart.html
-    } else {
-      return false; // Stay on current page
-    }
-  }
